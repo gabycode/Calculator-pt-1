7316913065329fa8485473680718ea1805b33257
gabycode <86427446+gabycode@users.noreply.github.com>
2022-01-14 16:17:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let num1 = 9
+let num2 = 3
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+let totalEl = document.getElementById("total-el")
+
+function add() {
+  let result = num1 + num2
+  totalEl.textContent = "Total = " + result 
+}
+
+function subtract() {
+  let result = num1 - num2
+  totalEl.textContent = "Total = " + result
+}
+
+function divide() {
+  let result = num1 / num2
+  totalEl.textContent = "Total = " + result
+}
+
+function multiply() {
+  let result = num1 * num2
+  totalEl.textContent = "Total = " + result
+}
